094425af827ab331bdff2ab33ab59655ce906d0d
Ubuntu <ubuntu@dev-1804.bos.berkshiregrey.com>
2021-04-01 16:58:54 -0400

    Improved await_test

--- await_test.py
+++ await_test.py
@@ -3,22 +3,32 @@ import asyncio
 
 # Test of asyncio gather() combined with a timeout feature
 
-async def test_func(info):
+# A function that sleeps for a random amount of time and might raise an exception
+async def test_func(info, raises_exception):
     print(">", info)
     await asyncio.sleep(random.randrange(5))
+    if raises_exception:
+        x = 7
+        bad = x / 0
     print("<", info)
     return info
 
-async def run_tasks():
+# Returns list where each entry contains results of task or whatever Exception was raised
+async def run_tasks(timeout):
     task_list = []
     for i in range(10):
-        task = asyncio.create_task(test_func("test "+str(i)))
+        raises_exception = (random.randrange(3) == 0)
+        task = asyncio.create_task(test_func("test "+str(i), raises_exception))
         task_list.append(task)
 
     results = []
     try:
+        # return_exceptions means exceptions will be returned as objects into
+        # results list, rather than thrown right away. This is helpful because
+        # we want all tasks to have a chance to finish. If result() is called
+        # on task, exception will be raised.
         gather_task = asyncio.gather(*task_list, return_exceptions=True)
-        results = await asyncio.wait_for(gather_task, 4)
+        results = await asyncio.wait_for(gather_task, timeout)
     except asyncio.TimeoutError:
         print("out of time!!!")
 
@@ -28,17 +38,27 @@ async def run_tasks():
         print("gather cancelled")
         for t in task_list:
             if t.cancelled():
-                results.append(None)
+                results.append(asyncio.TimeoutError())
             else:
-                results.append(t.result())
+                try:
+                    t.result()
+                except Exception as e:
+                    results.append(e)
+                else:
+                    results.append(t.result())
 
     return results
 
 async def run_test():
-    results = await run_tasks()
-    print("results:", results)
+    results = await run_tasks(timeout=3)
+    print("Results:")
+    for i,r in enumerate(results):
+        if isinstance(r, Exception):
+            print(f"{i}: is exception {type(r)}")
+        else:
+            print(f"{i}: {r}")
 
-    def _do_nothing():
+    async def _do_nothing():
         return None
 
     nothing_task = asyncio.create_task(_do_nothing())
